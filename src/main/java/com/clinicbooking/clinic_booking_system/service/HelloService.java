ecfe9d3a8cda32bfbd211f875d69faabade6c0d5
kietng04 <nguyenphantuankiet299@gmail.com>
2025-11-01 09:00:00 +0700

    setup project

---
+++ src/main/java/com/clinicbooking/clinic_booking_system/service/HelloService.java
@@ -0,0 +1,12 @@
+package com.clinicbooking.clinic_booking_system.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getHelloMessage() {
+        return "Hello Worldss";
+    }
+}
+

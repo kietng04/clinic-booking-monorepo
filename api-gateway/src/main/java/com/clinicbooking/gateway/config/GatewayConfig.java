4ef1cf519de53c9463f638657a2aca77071b2faa
kietng04 <nguyenphantuankiet299@gmail.com>
2026-02-11 12:46:51 +0000

    docs: add docker image migration instructions

--- api-gateway/src/main/java/com/clinicbooking/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/clinicbooking/gateway/config/GatewayConfig.java
@@ -34,9 +34,12 @@ public class GatewayConfig {
         // Allowed origins
         corsConfig.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
+                "http://127.0.0.1:3000",
                 "http://localhost:3001",
+                "http://127.0.0.1:3001",
                 "http://localhost:4200",
                 "http://localhost:5173",
+                "http://127.0.0.1:5173",
                 "http://localhost:8080"
         ));
 
